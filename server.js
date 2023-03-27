ffebf82212caa01a99e6e651e2a9b4d3fd61c596
amurillo18 <80285729+amurillo18@users.noreply.github.com>
2023-03-27 13:44:52 -0700

    added items

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express")
 const app = express()
 const morgan = require("morgan")
 const mongoose = require("mongoose")
+const path = require("path")
 
  const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 mongoose.set('strictQuery', true)
 mongoose.connect("mongodb+srv://Amurillo:Alex2015@cluster0.uvmcw4x.mongodb.net/ARKdb?retryWrites=true&w=majority", () => console.log('connected to database'))
@@ -17,6 +19,10 @@ app.use((err, req, res, next) => {
     console.log(err)
     return res.send({errMsg: err.message})
 })
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`)
